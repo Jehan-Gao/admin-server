4f726809ef6e70f56cc5653eaf9a1710deb8e87b
Jehan <gaozhiheng_email@163.com>
2020-11-30 18:15:14 +0800

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const router = require('koa-router')()
+
+
+router.get('/getUserInfo', function (ctx, next) {
+  ctx.body = JSON.stringify({
+    code: 0, 
+    msg: 'success',
+    data: 'this is getUserInfo data'
+  })
+})
+
+router.post('/saveUserInfo', function (ctx, next) {
+  ctx.status = 500
+  ctx.body = JSON.stringify({
+    code: 1,
+    msg: 'error',
+    data: 'this is saveUserInfo data'
+  })
+})
+
+module.exports = router
